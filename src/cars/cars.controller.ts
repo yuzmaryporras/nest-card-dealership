ae4042a7d42755f10103053ab288f0b7668c34cf
yuzmaryporras <yuzmaporras@gmail.com>
2025-04-23 20:55:01 -0400

    feat: New proyect to learn nest

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('cars')
+export class CarsController {
+
+    private cars = ['Toyota', 'Honda', 'Jeep'];
+
+    @Get()
+    getAllCars(){
+        return this.cars;
+    }
+
+    @Get(':id')
+    getCarById( @Param('id') id: string){
+        return this.cars[id];
+        
+    }
+}
